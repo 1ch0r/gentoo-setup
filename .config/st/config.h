96dfa7dfe43247a51f2f9e619a98511c6d1a41d1
1ch0r <95602383+1ch0r@users.noreply.github.com>
2025-10-28 11:06:49 -0400

    Add files via upload

--- .config/st/config.h
+++ .config/st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true" };
+static char *font = "Tiposka Dev:style=Regular:pixelsize=12";
+static char *font2[] = { "Tiposka Dev:style=Regular:pixelsize=12" };
 static int borderpx = 0;
 
 /*
